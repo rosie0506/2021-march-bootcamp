38b9e5c9b607a4514366ecebf64bc2f90ea71b6c
rosie0506 <81542906+rosie0506@users.noreply.github.com>
2021-04-08 16:18:37 +0800

    Update and rename assignment1.py to assignment1-Ruoxi.py

--- week1-introduction/assignment1/assignment1-Ruoxi.py
+++ week1-introduction/assignment1/assignment1-Ruoxi.py
@@ -7,58 +7,48 @@
 
 # Q1. Write a program which can compute the factorial of a given numbers.
 
-
 def factorial(x: int) -> int:
-    return 1
-
-
-assert factorial(0) == 1
-assert factorial(1) == 1
-assert factorial(9) == 362880
+    fac = 1
+    for i in range(1,x+1):
+       fac *= i
+    return fac
 
 
 # Q2. Write a program which take a num and print a str as the sum of all numbers from 1 to this number
 # [1 + 2 + ... + x] and x is always >= 1.
 
 def print_sum(x: int) -> str:
-    return ""
-
-
-assert print_sum(1) == "1"
-assert print_sum(3) == "6"
-assert print_sum(5) == "15"
+    sum = 0
+    for i in range(1, x+1):
+        sum += i
+    return str(sum)
 
 
 # Q3. Write a program to check is a year is leap year (x is always > 0)
 
 def is_leap_year(year: int) -> bool:
-    return False
-
-
-assert is_leap_year(2000)
-assert is_leap_year(1996)
-assert not is_leap_year(1900)
-assert not is_leap_year(2001)
+    if year % 400 ==0 or (year % 4 ==0 and year % 100 !=0):
+        return True
+    else:
+        return False
 
 
 # Q4. Write a program to convert a list of lowercase words to uppercase words.
 
 def to_upper_case(words: [str]) -> [str]:
-    return []
-
-
-assert to_upper_case(["abc", "de"]) == ["ABC", "DE"]
-assert to_upper_case(["Amazon", "Apple"]) == ["AMAZON", "APPLE"]
+    WORDS=[]
+    for c in words:
+        WORDS.append(c.upper())
+    return WORDS
 
 
 # Q5. Write a program to use only 'and' and 'or' to implement 'xor'
 # https://baike.baidu.com/item/%E5%BC%82%E6%88%96/10993677?fromtitle=xor&fromid=64178
 
 def xor(a: bool, b: bool) -> bool:
-    return False
-
-
-assert not xor(True, True)
-assert xor(True, False)
-assert xor(False, True)
-assert not xor(False, False)
+    if a and b:
+        return False
+    elif a or b:
+        return True
+    else:
+        return False
